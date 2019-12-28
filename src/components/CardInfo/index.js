cbc591d43189de186accb23d9ca2ba21e84ed40e
Rahul Jadhav <rahul@joshsoftware.com>
2019-12-28 19:21:09 +0530

    Added SCSS to show images on orderely fashion

--- src/components/CardInfo/index.js
+++ src/components/CardInfo/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Row, Col, Avatar } from "antd";
+import { Avatar } from "antd";
 import "./card-info.scss";
 
 const CardInfo = ({ title, subTitle, description, ...props }) => {
@@ -7,19 +7,19 @@ const CardInfo = ({ title, subTitle, description, ...props }) => {
   return (
     <Fragment>
       <div key={title} className={`card-info ${className}`}>
-        { title && <div className="title border-bottom">{title}</div> }
+        { title && <div className="title">{title}</div> }
         { subTitle && <div className="sub-title">{subTitle}</div> }
         { description && <p align="justify" className="description">{description}</p> }
         { technologies && (
-          <Row>
+          <ul className="technology-images">
             {
               technologies.split(", ").map((technology) => (
-                <Col key={technology}>
-                  <Avatar src={require(`../../assets/images/${technology}.svg`)} shape="square" alt={technology}/>
-                </Col>
+                <li>
+                  <Avatar src={require(`../../assets/images/${technology}.svg`)} shape="square" alt={technology} />
+                </li>
               ))
             }
-          </Row>)
+          </ul>)
         }
       </div>
     </Fragment>
